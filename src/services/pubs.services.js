f92db69e908a98c5f05fe4752b20d3a4fba229c0
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-06 18:53:22 +0200

    Revert

--- src/services/pubs.services.js
+++ src/services/pubs.services.js
@@ -19,25 +19,6 @@ function pubsOpenedToday() {
     return barsByDay;
 }
 
-<<<<<<< HEAD
-//JSON typé
-function initPubsArray(jsonPubsArrray) {
-    var pubsArray = [];
-
-    jsonPubsArray.forEach(pub => {
-        console.log(pub);
-        var owner = new Owner(pub.owner.firstName, pub.owner.lastName, pub.owner.mail);
-
-            pub.beers.forEach(beer => {
-                var biere = new Beer(beer.type, beer.name);
-                bieres.push(biere);
-            });
-
-            opendays = pub.openDays;
-            openhour = new OpenHour(pub.openHours.start, pub.openHours.end);
-
-        var bar = new Pub(pub._name, owner, opendays, openhour, bieres);
-=======
 function makePubsArrayFromJSON(jsonPubsArrray){
     var pubsArray = [];
 
@@ -55,61 +36,13 @@ function makePubsArrayFromJSON(jsonPubsArrray){
         var openhour = new OpenHour(pub.openHours.start, pub.openHours.end);
 
         var bar = new Pub(pub.name, owner, opendays, openhour, bieres);
->>>>>>> parent of ec50e84... Fin TP IHM
         pubsArray.push(bar);
     });
 
     return pubsArray;
 }
 
-<<<<<<< HEAD
-//JSON non typé
-function makePubsArrayFromJSON(jsonPubsArrray){
-    //Cette fonction est aussi appelée pour créer des pubs à partir du localStorage
-    //donc dans ce cas les propriétés beers, opendays et openhour ne seront pas définies
-    //ce qui causera une nullPointerException si on essaye de lire leurs propriétés
-
-    var pubsArray = [];
-    if(jsonPubsArrray != null)
-    {
-        jsonPubsArray.forEach(pub => {
-            var owner = new Owner(pub._owner._firstName, pub._owner._lastName, pub._owner._mail);
-    
-                if(pub._beers != null)
-                {
-                    pub._beers.forEach(beer => {
-                        var biere = new Beer(beer._type, beer._name);
-                        bieres.push(biere);
-                    });
-                }
-                
-                var opendays = null;
-                if(pub._openDays != null)
-                {
-                    opendays = pub._openDays;
-                }
-                
-                var openhour = null;
-                if(pub._openHours != null)
-                {
-                    openhour = new OpenHour(pub._openHours.start, pub._openHours.end);
-                }
-                
-            var bar = new Pub(pub._name, owner, opendays, openhour, bieres);
-            pubsArray.push(bar);
-        });
-    }
-
-    return pubsArray;
-}
-
-export default {
-    allPubs,
-    pubsOpenedToday,
-    makePubsArrayFromJSON
-=======
 export default { 
     allPubs, 
     pubsOpenedToday 
->>>>>>> parent of ec50e84... Fin TP IHM
 }
